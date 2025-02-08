3088cf33d29a7bf3b5bd0aa11d351c537f2e83b8
javiercervilla <jcervill@students.42madrid.com>
2025-02-08 16:10:42 +0000

    feat(DOCS):[+] Add updated docs for orders

--- src/loaders/docs.ts
+++ src/loaders/docs.ts
@@ -9,46 +9,6 @@ export async function docsLoader(app: Express) {
             res.sendFile('generate/openapi.json', { root: './' });
         });
         app.get('/docs', (_req: Request, res: Response) => {
-            //res.send(`
-            //    <!DOCTYPE html>
-            //    <html>
-            //    <head>
-            //        <title>Documentación de la API</title>
-            //        <link rel="icon" href="https://firemints.xyz/fire.svg" />
-            //        <script type="module" src="https://unpkg.com/rapidoc/dist/rapidoc-min.js"></script>
-            //    </head>
-            //    <body>
-            //        <!-- Componente RapiDoc -->
-            //        <rapi-doc
-            //            spec-url="/openapi.json"
-            //            render-style="interactive"
-            //            show-header="true"
-            //            show-info="true"
-            //            show-component-summary="true"
-            //            expand-responses="200,201"
-            //            theme='{
-            //                "colors": {
-            //                    "primary": {
-            //                        "main": "#2c3e50"
-            //                    },
-            //                    "background": {
-            //                        "main": "#ffffff"
-            //                    }
-            //                }
-            //            }'
-            //        >
-            //            <img
-            //                slot="logo"
-            //                src="https://firemints.xyz/fire.svg"
-            //            />
-            //            <img
-            //                slot="nav-logo"
-            //                src="https://firemints.xyz/logo.svg"
-            //            />
-            //        </rapi-doc>
-            //    </body>
-            //    </html>
-            //`);
             res.send(`
             <!DOCTYPE html>
             <html lang="en">
