9c8a76b950fc0129a9c00664d2801074dc8da2ec
javiercervilla <jcervill@students.42madrid.com>
2025-02-25 17:03:09 +0000

    feat(Routines): check-orders routine improved to be async using Promise.all

--- src/services/routines/check-orders.ts
+++ src/services/routines/check-orders.ts
@@ -9,19 +9,26 @@ const CHECK_INTERVAL_MS = 5000;
 async function checkOrder(order: OpenBookListing) {
     const { utxo } = order;
     const [txid, vout] = utxo.split(':');
-    cronLogger.info(`Checking order ${order.txid.slice(0, 6)}...${order.txid.slice(-6)} with UTXO ${utxo.slice(0, 6)}...${utxo.slice(-6)}`);
+    const orderIdSnippet = `${order.txid.slice(0, 6)}...${order.txid.slice(-6)}`;
+    const utxoSnippet = `${utxo.slice(0, 6)}...${utxo.slice(-6)}`;
+
+    cronLogger.info(`Checking order ${orderIdSnippet} with UTXO ${utxoSnippet}`);
+    
     let checked = false;
     while (!checked) {
         try {
             const tx = await rpc.getTXOUT(txid, Number(vout));
             if (!tx) {
-                cronLogger.error(`TXOUT not found for order ${order.txid.slice(0, 6)}...${order.txid.slice(-6)}. UTXO ${utxo.slice(0, 6)}...${utxo.slice(-6)} has been spent`);
+                cronLogger.error(`TXOUT not found for order ${orderIdSnippet}. UTXO ${utxoSnippet} has been spent`);
                 await Order.updateOpenbookListing(order.txid, 'inactive');
+                checked = true;
+            } else {
+                cronLogger.info(`Order ${orderIdSnippet} with UTXO ${utxoSnippet} is still active`);
                 checked = true;
             }
         } catch (error) {
-            cronLogger.error(`Error checking order ${order.txid.slice(0, 6)}...${order.txid.slice(-6)}. UTXO ${utxo.slice(0, 6)}...${utxo.slice(-6)}: ${error}`);
-            await new Promise((resolve) => setTimeout(resolve, CHECK_INTERVAL_MS));
+            cronLogger.error(`Error checking order ${orderIdSnippet}. UTXO ${utxoSnippet}: ${error}`);
+            await delay(CHECK_INTERVAL_MS);
         }
     }
 }
@@ -30,17 +37,22 @@ async function checkOrders() {
     try {
         const start = new Date();
         cronLogger.info(`[${start.toISOString()}] CHECK ORDERS CRON STARTED`);
+        
         const orders = await Order.getOpenbookListingsByStatus('active');
-        if (!orders) {
-            throw new Error("No orders found to check");
+        if (!orders || orders.length === 0) {
+            cronLogger.warn("No active orders found to check");
+            return;
         }
-        cronLogger.info(`[${new Date().toISOString()}] Connected to database. ${orders?.length} orders to check`);
-        for (const order of orders) {
-            await checkOrder(order);
-        }
+
+        cronLogger.info(`[${new Date().toISOString()}] Connected to database. ${orders.length} orders to check`);
+        await Promise.all(orders.map(order => checkOrder(order)));
     } catch (error) {
-        cronLogger.error(error);
+        cronLogger.error(`Error in checkOrders: ${error}`);
     }
+}
+
+function delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 Deno.cron("check-orders", CONFIG.CRON.CHECK_ORDERS_CRON, checkOrders);
