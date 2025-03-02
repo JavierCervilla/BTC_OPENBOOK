86fab77de9294a61c1db9984d46463f5d946ef4e
javiercervilla <jcervill@students.42madrid.com>
2025-03-02 22:32:14 +0000

    feat(LOGS):[+] Add critical error logger

--- src/services/indexer/indexer.ts
+++ src/services/indexer/indexer.ts
@@ -12,7 +12,7 @@ export async function initializeIndexer(db: Database) {
     logger.debug(`[${new Date().toISOString()}] Starting sync: Next block: ${block}, End block: ${endBlock}`);
 
     rpc.subscribeToMempoolSpaceWebSocket(["blocks"], {
-        onMessage: async(message) => {
+        onMessage: async (message) => {
             if (message.block) {
                 endBlock = Math.max(endBlock, message.block.height);
                 logger.info(`New block detected via WebSocket the tip now is ${message.block.height}`);
@@ -23,33 +23,37 @@ export async function initializeIndexer(db: Database) {
             endBlock = await rpc.getBlockCount();
             logger.info(`WebSocket connected. [${endBlock}]`);
         },
-        onError: (_error) => {},
-        onClose: () => {}
+        onError: (_error) => { },
+        onClose: () => { }
     });
 
     while (true) {
-        while (block <= endBlock) {
-            const start = new Date();
-            try {
-                const blockInfo = await rpc.getBlock(block);
-                const { atomic_swaps, openbook_listings } = await parser.parseBlock(db, blockInfo);
-                const end = new Date();
-                logger.info(
-                    `[${end.toISOString()}] Block ${block} / ${endBlock} (${atomic_swaps.length + openbook_listings.length} Txs - ${atomic_swaps.length} Swaps - ${openbook_listings.length} Listings) [${(
-                        (end.getTime() - start.getTime()) /
-                        1000
-                    ).toFixed(3)}s]`
-                );
-                block++;
-            } catch (error: unknown) {
-                logger.error(`Error processing block ${block}: ${error}`);
-                await new Promise((resolve) => setTimeout(resolve, 5000));
+        try {
+            while (block <= endBlock) {
+                const start = new Date();
+                try {
+                    const blockInfo = await rpc.getBlock(block);
+                    const { atomic_swaps, openbook_listings } = await parser.parseBlock(db, blockInfo);
+                    const end = new Date();
+                    logger.info(
+                        `[${end.toISOString()}] Block ${block} / ${endBlock} (${atomic_swaps.length + openbook_listings.length} Txs - ${atomic_swaps.length} Swaps - ${openbook_listings.length} Listings) [${(
+                            (end.getTime() - start.getTime()) /
+                            1000
+                        ).toFixed(3)}s]`
+                    );
+                    block++;
+                } catch (error: unknown) {
+                    logger.error(`Error processing block ${block}: ${error}`);
+                    await new Promise((resolve) => setTimeout(resolve, 500));
+                }
             }
-        }
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-        logger.info(`[${new Date().toISOString()}] No new blocks to process. Waiting for updates...`);
-        while (block > endBlock) {
             await new Promise((resolve) => setTimeout(resolve, 1000));
+            logger.info(`[${new Date().toISOString()}] No new blocks to process. Waiting for updates...`);
+            while (block > endBlock) {
+                await new Promise((resolve) => setTimeout(resolve, 1000));
+            }
+        } catch (error) {
+            logger.critical(error);
         }
     }
 }
@@ -59,7 +63,6 @@ export async function start(db: Database) {
     try {
         InitialPrompt();
         await initializeIndexer(db);
-
     } catch (error) {
         logger.critical(error);
     }
